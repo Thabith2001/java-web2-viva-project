cd34cde88430c27dc09760cf33573f1a82838713
Thabith2001 <129141730+Thabith2001@users.noreply.github.com>
2023-09-18 12:49:12 +0530

    Update AppConfig.java

--- src/main/java/com/ebookstore/config/AppConfig.java
+++ src/main/java/com/ebookstore/config/AppConfig.java
@@ -6,8 +6,8 @@ import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 public class AppConfig extends ResourceConfig {
 
     public AppConfig(){
-        packages("com.taita.web.ebookstore.controller");
-        packages("com.taita.web.ebookstore.middleware");
+        packages("com.ebookstore.controller");
+        packages("com.ebookstore.middleware");
 
         register(JspMvcFeature.class);
         property(JspMvcFeature.TEMPLATE_BASE_PATH,"/WEB-INF/views");
